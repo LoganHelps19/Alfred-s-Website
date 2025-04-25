3b44e2c7d4c68f9cdfcfde9708c76537dbf30e7e
LoganHelps19 <40018357@petroc.ac.uk>
2025-04-25 09:32:53 +0000

    inital commit IT

---
+++ pythonwork.py
@@ -0,0 +1,35 @@
+#Python Basics Worksheet: Space Dinosaurs Edition! 
+SPACESHIP_NAME = "astro"
+fuel_level = "high"
+crew_size = "100"
+planet = "Earth"
+
+time_to_launch = "T-minus " + 5 + " seconds"
+print("🚀 Welcome aboard the", "ASTRO_SHIP", "with", "100", "dinosaurs!")
+
+fuel_level = 75  # Change this value to test different outcomes!
+
+if fuel_level >= 80:
+    print("🦖✅ Plenty of fuel! Launching in 3...2...1...🚀")
+elif fuel_level >= 40: 
+    print("🛑⚠️ Fuel is a bit low. Consider refueling before launch.")
+else:
+    print("❌ Not enough fuel! Mission aborted!")
+
+    print("🌟 Counting stars:")
+for i in range (10):
+    print( "there are 2 stars 🌟🌟")
+
+fuel = 50  # Current fuel percentage
+
+while ( fuel_level +'50' ):
+    print("🛸 Floating through space... Fuel left:", fuel, "%")
+    fuel -= 10
+
+print("🚨 Out of fuel! Time to refuel!")
+
+dino_crew = []  # Add your dino astronauts here!
+
+print("👨‍🚀 Meet the Jurassic Space Crew!")
+print("🦕 Captain",)
+
